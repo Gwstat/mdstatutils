869e070ce13fed2390ea5404af09373358949f7f
Georg Wiegleb <gwiestat@gmail.com>
2023-02-03 13:20:13 +0100

    nextcloud_path() - Kleinschreibung der Funktion

--- R/nextcloud_path.R
+++ R/nextcloud_path.R
@@ -1,4 +1,4 @@
-Nextcloud_Path <- function(x) {
+nextcloud_path <- function(x) {
   Userdir <- paste0("C:/Users/", as.character(Sys.info()[which(names(Sys.info()) == "effective_user")]))
   Ncloudname <- list.files(Userdir)[which(startsWith(list.files(Userdir), "Nextcloud"))] |> tail(1)
   Nextcloud_Pfad <- paste0(Userdir, "/", Ncloudname)
