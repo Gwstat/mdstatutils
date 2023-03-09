8fdd8ca0bc263e4266e5c43349e7072a3464e6db
Georg Wiegleb <gwiestat@gmail.com>
2023-03-09 18:55:05 +0100

    test

--- R/area_prop_on_point.R
+++ R/area_prop_on_point.R
@@ -3,16 +3,12 @@ area_prop_on_point <- function(Points,
                                radius = 1000,
                                proportion = TRUE,
                                pb = TRUE) {
-
-
   Points |>
     sf::st_buffer(radius) |>
     sf::st_intersection(Areas) |>
-    # spawn_progressbar() |>
     (\(x) {
       x$area <- sf::st_area(x)
       x
     })()
-
 
 }
